33507f614db0dd321df595194f092f98300b7a18
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-11-05 22:49:39 +0300

    Create User Repository

---
+++ traveler-backend/src/main/java/com/ieg/traveler/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ieg.traveler.repository;
+
+import com.ieg.traveler.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User getById(int id);
+
+    User getUserByUserName(String userName);
+    
+}
