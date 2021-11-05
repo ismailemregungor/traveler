5b0f21005fb0b5e9ea0f7b4690427b1bd8a8b18f
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-11-05 22:50:35 +0300

    Create User Service

---
+++ traveler-backend/src/main/java/com/ieg/traveler/service/UserService.java
@@ -0,0 +1,23 @@
+package com.ieg.traveler.service;
+
+import com.ieg.traveler.model.user.User;
+import com.ieg.traveler.util.response.DataResponse;
+import com.ieg.traveler.util.response.Response;
+import java.util.List;
+public interface UserService {
+
+     Response add(User user);
+
+     Response update(User user);
+
+     Response delete(int id);
+
+     DataResponse<List<User>> getAll();
+
+     DataResponse<User> getById(int id);
+
+     DataResponse<User> getUserByUserName(String userName);
+
+
+
+}
