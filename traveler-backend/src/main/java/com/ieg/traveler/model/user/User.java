fda069a1ea7df61aefc5e029757a0532aaed22e0
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-02-01 16:24:21 +0300

    Set some restrictions for user registration and show warning message to users based on these restrictions

--- traveler-backend/src/main/java/com/ieg/traveler/model/user/User.java
+++ traveler-backend/src/main/java/com/ieg/traveler/model/user/User.java
@@ -1,9 +1,13 @@
 package com.ieg.traveler.model.user;
 
+import com.ieg.traveler.util.constraint.UniqueUsername;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,13 +21,20 @@ public class User {
     @Column(name = "id")
     private int id;
 
+    @Size(min = 2, max = 255)
+    @NotNull(message = "{traveler.constraints.displayName.NotNull.message}")
     @Column(name = "display_name")
     private String displayName;
 
+    @UniqueUsername
+    @Size(min = 4, max = 255)
+    @NotNull(message = "{traveler.constraints.userName.NotNull.message}")
     @Column(name = "user_name")
     private String userName;
 
+    @Size(min = 6, max = 255)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{traveler.constraints.password.Pattern.message}")
+    @NotNull(message = "{traveler.constraints.password.NotNull.message}")
     @Column(name = "password")
     private String password;
-
 }
