daaf3d55bb99ca21032db58c2fcfa2f97e1cba21
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-02-01 17:15:32 +0300

    Send API call to backend for users to sign up

--- traveler-frontend/src/services/userService.jsx
+++ traveler-frontend/src/services/userService.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const signUp = (body) => {
-  return axios.post('/api/1.0/users', body);
+  return axios.post('/api/1.0/users/add', body, {
+    headers: { 'accept-language': 'tr' },
+  });
 };
