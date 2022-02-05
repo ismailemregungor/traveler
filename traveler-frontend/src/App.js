9714373924acfe535856d416746c1464a442c247
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-02-05 18:02:49 +0300

    Create initial react environment for traveler

--- traveler-frontend/src/App.js
+++ traveler-frontend/src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import UserSignUp from './pages/UserSignUp.jsx';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <UserSignUp></UserSignUp>
+    </div>
+  );
 }
 
 export default App;
