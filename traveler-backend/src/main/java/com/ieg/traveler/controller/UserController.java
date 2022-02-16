67fbda0c2c61cd755d323c2a696338d9f7a6afdc
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-02-16 23:49:44 +0300

    Update User Controller

--- traveler-backend/src/main/java/com/ieg/traveler/controller/UserController.java
+++ traveler-backend/src/main/java/com/ieg/traveler/controller/UserController.java
@@ -2,18 +2,12 @@ package com.ieg.traveler.controller;
 
 import com.ieg.traveler.model.user.User;
 import com.ieg.traveler.service.UserService;
-import com.ieg.traveler.util.response.ApiError;
 import com.ieg.traveler.util.response.DataResponse;
 import com.ieg.traveler.util.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/1.0/users")
@@ -31,18 +25,6 @@ public class UserController {
     @PostMapping("/add")
     public Response add(@Valid @RequestBody User user) {
         return this.userService.add(user);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleValidationException(MethodArgumentNotValidException methodArgumentNotValidException){
-        ApiError apiError = new ApiError(400, "Validation Error", "/api/1.0/users");
-        Map<String, String> validationErrors = new HashMap<>();
-        for(FieldError fieldError : methodArgumentNotValidException.getBindingResult().getFieldErrors()){
-            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-        apiError.setValidationErrors(validationErrors);
-        return apiError;
     }
 
     @PostMapping("/update")
