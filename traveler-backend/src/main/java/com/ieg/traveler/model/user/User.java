de3e1776911b97e6a16d0664645f5eab0ad4d4ce
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-02-26 19:45:46 +0300

    Implement and customize the UserDetails

--- traveler-backend/src/main/java/com/ieg/traveler/model/user/User.java
+++ traveler-backend/src/main/java/com/ieg/traveler/model/user/User.java
@@ -4,17 +4,22 @@ import com.ieg.traveler.util.constraint.UniqueUsername;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import java.util.Collection;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,4 +42,36 @@ public class User {
     @NotNull(message = "{traveler.constraints.password.NotNull.message}")
     @Column(name = "password")
     private String password;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList( "Role_user");
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
