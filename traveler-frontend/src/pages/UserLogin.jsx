35c831b25e627cf990cc47a79abc0a80ebeae715
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-03-05 15:03:50 +0300

    Create a login page for users, inform them about ongoing transactions, add multi-language support

--- traveler-frontend/src/pages/UserLogin.jsx
+++ traveler-frontend/src/pages/UserLogin.jsx
@@ -1,42 +1,99 @@
 import React, { Component } from 'react';
 import { Input } from '../components/Input.jsx';
 import { withTranslation } from 'react-i18next';
+import { login } from '../services/userService';
+import axios from 'axios';
+import ButtonWithProgress from '../components/ButtonWithProgress.jsx';
 
 class UserLogin extends Component {
   state = {
     userName: null,
     password: null,
+    error: null,
+    pendingApiCall: false,
   };
+
+  componentDidMount() {
+    axios.interceptors.request.use((request) => {
+      this.setState({ pendingApiCall: true });
+      return request;
+    });
+
+    axios.interceptors.response.use(
+      (response) => {
+        this.setState({ pendingApiCall: false });
+        return response;
+      },
+      (error) => {
+        this.setState({ pendingApiCall: false });
+        throw error;
+      }
+    );
+  }
 
   onChange = (event) => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
+      error: null,
     });
+  };
+
+  onClickLogin = async (event) => {
+    event.preventDefault();
+    const { userName, password } = this.state;
+    const creds = {
+      userName,
+      password,
+    };
+    this.setState({
+      error: null,
+    });
+    try {
+      await login(creds);
+    } catch (apiError) {
+      this.setState({
+        error: apiError.response.data.message,
+      });
+    }
   };
 
   render() {
     const { t } = this.props;
+
+    const { userName, password, error, pendingApiCall } = this.state;
+
+    const buttonEnabled = userName && password;
+
     return (
       <div className="container">
         <form>
           <h1 className="text-center">{t('Login')}</h1>
+
           <Input
             name="userName"
             label={t('User Name')}
-            //error={userName}
             onChange={this.onChange}
             type="userName"
           ></Input>
+
           <Input
             name="password"
             label={t('Password')}
-            //error={password}
             onChange={this.onChange}
             type="password"
           ></Input>
+          <br />
+
+          {error && <div className="alert alert-danger">{error}</div>}
+
           <div className="button">
-            <button className="btn btn-primary">{t('Login')}</button>
+            <ButtonWithProgress
+              onClick={this.onClickLogin}
+              disabled={!buttonEnabled || pendingApiCall}
+              pendingApiCall={pendingApiCall}
+              text={t('Login')}
+            ></ButtonWithProgress>
           </div>
         </form>
       </div>
